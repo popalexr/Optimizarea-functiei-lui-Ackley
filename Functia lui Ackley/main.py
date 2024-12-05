ad128992964ab3a734e3422add2c3a400fb89037
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-12-06 00:57:21 +0200

    Remove .env.example file, add performance data logging to JSON, and measure execution time in main.py

--- Functia lui Ackley/main.py
+++ Functia lui Ackley/main.py
@@ -1,13 +1,18 @@
 import os
 import dotenv
+import time
+import json
 
 from classes.GA import GA
 from classes.Plot import Plot
 
+
+
+
 if __name__ == "__main__":
     # Load the environment variables
     dotenv.load_dotenv()
-
+    time_start = time.perf_counter()
     genetical_algorithm = GA(
         dimension=int(os.getenv('DIMENSION')),
         initial_population=int(os.getenv('INITIAL_POPULATION')),
@@ -18,6 +23,24 @@ if __name__ == "__main__":
 
     print(f"Best specimen: {genetical_algorithm.get_best_specimen().get_chromosome()} with fitness {genetical_algorithm.get_best_specimen().get_fitness()}")
 
+
+    best_fitnesses = genetical_algorithm.get_best_fitnesses_list()
+
+
+    performance_data = []
+    for i in range(len(best_fitnesses)):
+            performance_data.append({
+        "generation": i + 1,  # Using i as the generation index
+        "best_fitness": best_fitnesses[i],
+        
+    })
+
+# Save to JSON
+    with open("performance_data.json", mode="w") as file:
+        json.dump(performance_data, file, indent=4)
+
+    time_end = time.perf_counter()
+    print(f"Elapsed time: {time_end - time_start} seconds")
     # Plot the best fitnesses for each generation
     plot = Plot(genetical_algorithm.get_best_fitnesses_list())
     plot.plot()
